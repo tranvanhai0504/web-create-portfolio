c68d497cdb4ba449314d3ae5387ecde6762a479a
qui_giao_su <52100996@student.tdtu.edu.vn>
2022-11-29 01:13:05 +0700

    add switch mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,12 @@ import { memo, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import clsx from 'clsx'
 import styles from './Header.module.css'
+import './Header.css'
 import logo from '../../assets/logo.png'
 import MiniModal from './MiniModal/MiniModal'
 import { GlobalContext } from "../../globalState/GlobalState";
 import { useContext } from 'react'
-
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const listButtonHeader = [{ name: 'Home', href: "/pages/home" }, { name: 'Storage', href: "/pages/storage" }, { name: 'About', href: "/pages/about" }]
 
@@ -39,11 +40,23 @@ function Header() {
                         {btn.name === 'Storage' && <MiniModal />}
                     </div>)
                 })}
+            <label className="switch">
+                <input 
+                    onChange={()=>value.toggleTheme()}
+                    type='checkbox'
+                    checked={value.checked}
+                
+                />
+                
+                <span className="slider">
+                    <span className='lightBtn modeBtn'><FiSun /></span>
+                    <span className='dark modeBtn'><FiMoon /></span>
+                </span>
+            </label>
             </div>
             <div className={styles.btnFuncList}>
                 <NavLink to="/work" className={clsx(styles.btnStart)}>Get Started</NavLink>
             </div>
-            <button onClick={e=>value.toggleTheme()}>changemode</button>
         </div>
     )
 }
--- src/globalState/GlobalState.js
+++ src/globalState/GlobalState.js
@@ -5,15 +5,20 @@ const GlobalContext = createContext()
 
 function GlobalProvider({children}){
     const data = JSON.parse(localStorage.getItem('theme'))||'light'
+    const dataToggle = JSON.parse(localStorage.getItem('checked'))|| false
     const [theme, setTheme] = useState(data)
+    const [checked, setChecked] = useState(dataToggle)
     const toggleTheme = () => {
         setTheme(theme==='light'? 'dark' : 'light')
+        setChecked(!checked)
     }
-
     useEffect(()=>{
         localStorage.setItem('theme', JSON.stringify(theme))
+        localStorage.setItem('checked', JSON.stringify(checked))
+
     }, [theme])
     const value = {
+        checked,
         theme,
         toggleTheme
     }
